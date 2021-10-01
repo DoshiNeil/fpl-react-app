79bca825c7df14c5731e450cff53cd6c72df4a99
bpatro <bpatro@motorola.com>
2021-10-01 18:12:29 +0530

    Created routing.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import "./App.css";
+import LandingPage from "./LandingPage/LandingPage";
+import LoginPage from "./LoginPage/LoginPage";
+import HomePage from "./HomePage/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/">
+          <LandingPage />
+        </Route>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route path="/home">
+          <HomePage />
+        </Route>
+      </Switch>
     </div>
   );
 }
